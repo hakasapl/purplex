bb8e5c37b9b47d98f38f23b4c6ff14adf77728e4
hakasapl <40907639+hakasapl@users.noreply.github.com>
2020-01-31 00:23:22 -0500

    Got something working

--- README.md
+++
@@ -1,1 +0,0 @@
-# Purplex Mod #
---
+++ src/main/java/com/github/hakasapl/purplex/Purplex.java
@@ -0,0 +1,8 @@
+package com.github.hakasapl.purplex;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Purplex.MODID)
+public final class Purplex {
+    public static final String MODID = "purplex";
+}
