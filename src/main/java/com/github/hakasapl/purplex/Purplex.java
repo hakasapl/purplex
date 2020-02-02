ac082baa1cf74783b084f68880c8ee4d3506931e
hakasapl <40907639+hakasapl@users.noreply.github.com>
2020-02-01 22:12:24 -0500

    Updated girignore

--- src/main/java/com/github/hakasapl/purplex/Purplex.java
+++ src/main/java/com/github/hakasapl/purplex/Purplex.java
@@ -1,5 +1,9 @@
 package com.github.hakasapl.purplex;
 
+import net.minecraft.block.Block;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.Tag;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.Mod;
 
 import org.apache.logging.log4j.LogManager;
@@ -11,6 +15,15 @@ public final class Purplex {
     public static final Logger LOGGER = LogManager.getLogger(MODID);
 
     public Purplex() {
+        ResourceLocation myTag = new ResourceLocation("forge", "ores");
+        Tag<Block> blocks = BlockTags.getCollection().get(myTag);
+        if (blocks == null) {
+            LOGGER.debug("Null Ores");
+        } else {
+            for(Block b:blocks.getAllElements()) {
+                LOGGER.debug(b);
+            }
+        }
 
         LOGGER.debug("Mod Loaded");
     }
