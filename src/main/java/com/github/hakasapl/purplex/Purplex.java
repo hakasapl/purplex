847b5ce7c2c7afaa143a1e55932880d01324d429
jeremydrn <60497084+jeremydrn@users.noreply.github.com>
2020-02-01 23:59:34 -0500

    First WorldGen testing

--- src/main/java/com/github/hakasapl/purplex/Purplex.java
+++ src/main/java/com/github/hakasapl/purplex/Purplex.java
@@ -1,6 +1,12 @@
 package com.github.hakasapl.purplex;
 
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+import com.github.hakasapl.purplex.init.WorldGen;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,7 +17,12 @@ public final class Purplex {
     public static final Logger LOGGER = LogManager.getLogger(MODID);
 
     public Purplex() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
 
         LOGGER.debug("Mod Loaded");
+    }
+
+    public void setup(FMLCommonSetupEvent event){
+        WorldGen.onCommonSetup();
     }
 }
---
+++ src/main/java/com/github/hakasapl/purplex/init/WorldGen.java
@@ -0,0 +1,20 @@
+package com.github.hakasapl.purplex.init;
+
+import com.github.hakasapl.purplex.blocks.BlockPurpore;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.gen.GenerationStage;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.OreFeatureConfig;
+import net.minecraft.world.gen.placement.CountRangeConfig;
+import net.minecraft.world.gen.placement.Placement;
+import net.minecraftforge.registries.ForgeRegistries;
+
+
+public class WorldGen {
+    public static void onCommonSetup() {
+        ForgeRegistries.BIOMES.forEach(biome -> {
+            biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.func_225566_b_(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, PurplexBlocks.purpore.getDefaultState(), 8)).func_227228_a_(Placement.COUNT_RANGE.func_227446_a_(new CountRangeConfig(200, 0, 0, 16))));
+        });
+    }
+}
