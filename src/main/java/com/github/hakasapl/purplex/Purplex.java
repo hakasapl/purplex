c9c40974b0352b13051e812a13367d524d77f171
hakasapl <40907639+hakasapl@users.noreply.github.com>
2020-02-02 00:02:47 -0500

    Update loot table

--- src/main/java/com/github/hakasapl/purplex/Purplex.java
+++ src/main/java/com/github/hakasapl/purplex/Purplex.java
@@ -1,9 +1,10 @@
 package com.github.hakasapl.purplex;
 
-import net.minecraft.block.Block;
-import net.minecraft.tags.BlockTags;
-import net.minecraft.tags.Tag;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.storage.loot.LootPool;
+import net.minecraft.world.storage.loot.TableLootEntry;
+import net.minecraftforge.event.LootTableLoadEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 import org.apache.logging.log4j.LogManager;
@@ -15,16 +16,14 @@ public final class Purplex {
     public static final Logger LOGGER = LogManager.getLogger(MODID);
 
     public Purplex() {
-        ResourceLocation myTag = new ResourceLocation("forge", "ores");
-        Tag<Block> blocks = BlockTags.getCollection().get(myTag);
-        if (blocks == null) {
-            LOGGER.debug("Null Ores");
-        } else {
-            for(Block b:blocks.getAllElements()) {
-                LOGGER.debug(b);
-            }
-        }
-
         LOGGER.debug("Mod Loaded");
+    }
+
+    @SubscribeEvent
+    public static void onLootTableLoad(LootTableLoadEvent event) {
+        LOGGER.debug(event.getName());
+        if (event.getName().equals(new ResourceLocation("purplex", "purpore"))) {
+            event.getTable().addPool(LootPool.builder().addEntry(TableLootEntry.builder(new ResourceLocation("minecraft", "string"))).build());
+        }
     }
 }
--- src/main/java/com/github/hakasapl/purplex/blocks/BlockPurpore.java
+++ src/main/java/com/github/hakasapl/purplex/blocks/BlockPurpore.java
@@ -5,9 +5,12 @@ import com.github.hakasapl.purplex.Purplex;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.ToolType;
+import net.minecraftforge.event.LootTableLoadEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 
-public final class BlockPurpore extends Block {
+public class BlockPurpore extends Block {
     public static final Block.Properties PROPERTIES = Block.Properties.create(Material.ROCK, MaterialColor.GRAY).hardnessAndResistance(3.0F).harvestTool(ToolType.PICKAXE).lightValue(4);
 
     public static final String NAME = "purpore";
